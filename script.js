a64530bd734a4891710f2959220be0a0bdc7ca4e
s0linsky <solinskybruh@gmail.com>
2022-12-13 22:45:54 -0500

    Add makeGrid() function

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.getElementById("container");
+
+function makeGrid(rows, cols) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (i = 0; i < rows * cols; i++) {
+    let cell = document.createElement("div");
+    cell.innerText = i + 1;
+    container.appendChild(cell).className = "grid-item";
+  }
+}
